94a613bc4a080b0eafd2d492175fb35b7175060a
Formation-02 <Formation-02>
2016-02-02 17:23:05 +0100

    blablal

---
+++ App/js/app.js
@@ -0,0 +1,71 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+            controller: 'AppController',
+            templateUrl: 'templates/home.html'
+        })
+
+        .when('/about', {
+            controller: 'AboutController',
+            templateUrl: 'templates/about.html'
+        })
+
+        .when('/recruitement', {
+            controller: 'RecruitementController',
+            templateUrl: 'templates/recruitement.html'
+        })
+
+        .when('/quotation', {
+            controller: 'QuotationController',
+            templateUrl: 'templates/quotation.html'
+        })
+
+        .when('/contact', {
+            controller: 'ContactController',
+            templateUrl: 'templates/contact.html'
+        })
+
+        .when('/404', {
+            controller: 'NotFoundController',
+            templateUrl: 'templates/404.html'
+        })
+
+        .otherwise({redirectTo: '/404'})
+        ;
+});
+
+app.controller('AppController', ['$scope', function($scope){
+    console.log('AppController');
+}]);
+
+app.controller('AboutController', ['$scope', function($scope){
+    console.log('AboutController');
+}]);
+
+app.controller('RecruitementController', ['$scope', function($scope){
+    console.log('RecruitementController');
+}]);
+
+app.controller('QuotationController', ['$scope', function($scope){
+    console.log('QuotationController');
+}]);
+
+app.controller('ContactController', ['$scope', function($scope){
+    console.log('ContactController');
+}]);
+
+app.controller('NotFoundController', ['$scope', function($scope){
+    console.log('NotFoundController');
+}]);
+
+
+app.directive('maDirective', function() {
+  return {
+    replace: true,
+    restrict: 'A',
+    template: '<p>My new directive</p>'
+  }
+
+});
